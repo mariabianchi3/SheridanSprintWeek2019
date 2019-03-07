415775db0e1bd60e633a0efd0d48a1be8156d935
Maria <Bianchi>
2019-03-07 12:45:24 -0500

    ability buttons and mining toggle
    added separate buttons for different abilities and fixed the mining toggle to disable unwanted buttons

---
+++ Assets/MiningToggle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiningToggle : MonoBehaviour
+{
+    public MineGun mineGun;
+    public Button leftButton;
+    public Button rightButton;
+    public Button jumpButton;
+    public Button boostButton;
+    public Button craftButton;
+
+    public void toggleMining()
+    {
+        if (mineGun != null)
+        {
+            mineGun.isMining = !mineGun.isMining;
+
+            if(mineGun.isMining)
+            {
+                //leftButton.enabled = false;
+                leftButton.interactable = false;
+                rightButton.interactable = false;
+                jumpButton.interactable = false;
+                boostButton.interactable = false;
+                craftButton.interactable = false;
+            }
+            else
+            {
+                //leftButton.enabled = true;
+                leftButton.interactable = true;
+                rightButton.interactable = true;
+                jumpButton.interactable = true;
+                boostButton.interactable = true;
+                craftButton.interactable = true;
+            }
+        }
+    }
+}
--- Assets/ToggleAbility.cs
+++ Assets/ToggleAbility.cs
@@ -6,90 +6,119 @@ using UnityEngine.UI;
 public class ToggleAbility : MonoBehaviour
 {
 
-    public bool craft = true;
-    private Button abilitybutton;
-    public Color craftColor = Color.cyan;
-    public Color jumpColor = Color.magenta;
+    //public bool craft = true;
+    //private Button abilitybutton;
+    //public Color craftColor = Color.cyan;
+    //public Color jumpColor = Color.magenta;
 
     public PlayerControl pc;
 
-    public void Awake()
+    //public void Awake()
+    //{
+    //    abilitybutton = GetComponent<Button>();
+    //    if (abilitybutton != null)
+    //    {
+    //        if (craft)
+    //        {
+    //            ColorBlock cb = abilitybutton.colors;
+    //            cb.normalColor = craftColor;
+    //            cb.highlightedColor = craftColor;
+    //            abilitybutton.colors = cb;
+    //            //Debug.Log(abilitybutton.colors.normalColor);
+    //        }
+    //        else
+    //        {
+    //            ColorBlock cb = abilitybutton.colors;
+    //            cb.normalColor = jumpColor;
+    //            cb.highlightedColor = jumpColor;
+    //            abilitybutton.colors = cb;
+    //            //Debug.Log(abilitybutton.colors.normalColor);
+    //        }
+    //    }
+    //}
+    //public void toggleAbility()
+    //{
+    //    craft = !craft;
+    //    if (abilitybutton != null)
+    //    {
+    //        if (craft)
+    //        {
+    //            //abilitybutton.colors.normalColor = Color.blue;
+    //            ColorBlock cb = abilitybutton.colors;
+    //            cb.normalColor = craftColor;
+    //            cb.highlightedColor = craftColor;
+    //            abilitybutton.colors = cb;
+    //            //Debug.Log(abilitybutton.colors.normalColor);
+    //        }
+    //        else
+    //        {
+    //            ColorBlock cb = abilitybutton.colors;
+    //            cb.normalColor = jumpColor;
+    //            cb.highlightedColor = jumpColor;
+    //            abilitybutton.colors = cb;
+    //            //Debug.Log(abilitybutton.colors.normalColor);
+    //        }
+    //    }
+    //}
+
+
+    public void doBoostJump()
     {
-        abilitybutton = GetComponent<Button>();
-        if (abilitybutton != null)
+        //if(pc.mineGun.isMining)
+        //{
+        //    return;
+        //}
+        if (pc != null)
         {
-            if (craft)
+            if (pc.Grounded && pc.RVal.RedP > 10)
             {
-                ColorBlock cb = abilitybutton.colors;
-                cb.normalColor = craftColor;
-                cb.highlightedColor = craftColor;
-                abilitybutton.colors = cb;
-                //Debug.Log(abilitybutton.colors.normalColor);
-            }
-            else
-            {
-                ColorBlock cb = abilitybutton.colors;
-                cb.normalColor = jumpColor;
-                cb.highlightedColor = jumpColor;
-                abilitybutton.colors = cb;
-                //Debug.Log(abilitybutton.colors.normalColor);
-            }
-        }
-    }
-    public void toggleAbility()
-    {
-        craft = !craft;
-        if (abilitybutton != null)
-        {
-            if (craft)
-            {
-                //abilitybutton.colors.normalColor = Color.blue;
-                ColorBlock cb = abilitybutton.colors;
-                cb.normalColor = craftColor;
-                cb.highlightedColor = craftColor;
-                abilitybutton.colors = cb;
-                //Debug.Log(abilitybutton.colors.normalColor);
-            }
-            else
-            {
-                ColorBlock cb = abilitybutton.colors;
-                cb.normalColor = jumpColor;
-                cb.highlightedColor = jumpColor;
-                abilitybutton.colors = cb;
-                //Debug.Log(abilitybutton.colors.normalColor);
+                pc.playerRB.AddForce(transform.up * pc.Boost);
+                pc.RVal.RedP -= 10;
             }
         }
     }
 
-    public void useAbility()
+    public void doCraft()
     {
-        if (pc.mineGun.isMining)
+        if (pc != null)
         {
-            //Debug.Log("ismining");
-        }
-        else
-        {
-            if (craft)
+            if (pc.RVal.BlueP > 10)
             {
-                if(pc != null){
-                    if (pc.RVal.BlueP > 10)
-                    {
-                        pc.mineGun.Craft();
-                        pc.RVal.BlueP -= 10;
-                    }
-                }
-            }
-            else
-            {
-                if(pc != null)
-                {
-                    if(pc.Grounded && pc.RVal.RedP > 10)
-                    {
-                        pc.playerRB.AddForce(transform.up * pc.Boost);
-                        pc.RVal.RedP -= 10;
-                    }
-                }
+                pc.mineGun.Craft();
+                pc.RVal.BlueP -= 10;
             }
         }
     }
+
+    //public void useAbility()
+    //{
+    //    if (pc.mineGun.isMining)
+    //    {
+    //        //Debug.Log("ismining");
+    //    }
+    //    else
+    //    {
+    //        if (craft)
+    //        {
+    //            if(pc != null){
+    //                if (pc.RVal.BlueP > 10)
+    //                {
+    //                    pc.mineGun.Craft();
+    //                    pc.RVal.BlueP -= 10;
+    //                }
+    //            }
+    //        }
+    //        else
+    //        {
+    //            if(pc != null)
+    //            {
+    //                if(pc.Grounded && pc.RVal.RedP > 10)
+    //                {
+    //                    pc.playerRB.AddForce(transform.up * pc.Boost);
+    //                    pc.RVal.RedP -= 10;
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 }
