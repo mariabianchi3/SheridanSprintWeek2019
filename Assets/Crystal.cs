a658d7cea3fc22435274324b88d681e2b514c89d
Maria <Bianchi>
2019-03-07 10:24:50 -0500

    Initial Commit
    intial commit with current game

---
+++ Assets/Crystal.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    public float rockHealth;
+    public float hitDmg;
+    public bool Hit;
+    public float cooldown = 2;
+    public SpriteRenderer crystalImg;
+    public GameObject RedPrefab;
+    public GameObject BluePrefab;
+    private Vector3 ScatterPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hit = false;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+        if (rockHealth <= 0)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                ScatterPos = new Vector3(transform.position.x + Random.Range(-1, 1), transform.position.y + Random.Range(0, 1), transform.position.z);
+                Instantiate(RedPrefab, ScatterPos, transform.rotation);
+            }
+
+            Destroy(this.gameObject);
+        }
+
+        if (Hit)
+        {
+            cooldown -= 0.2f;
+        }
+
+        if(cooldown <= 0)
+        {
+            crystalImg.color = Color.white;
+            Hit = false;
+            cooldown = 2;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            RockHit();
+            DoCool();
+            //cooldown -= 0.2f * Time.deltaTime;
+
+        }
+    }
+
+    void RockHit()
+    {
+        Color OrangeGlow = new Color(255, 182, 0, 68);
+        rockHealth -= hitDmg;
+        Hit = true;
+        crystalImg.color = OrangeGlow;
+        //cooldown -= 0.2f * Time.deltaTime;
+
+
+
+    }
+    void DoCool()
+    {
+        cooldown -= 0.2f;
+    }
+
+
+}
---
+++ Assets/Scripts/TouchExample.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchExample : MonoBehaviour
+{
+    public bool isMining = false;
+    public float jumpForce = 200.0f;
+    private Rigidbody rb;
+    private Vector3 position;
+    private float width;
+    private float height;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        width = (float)Screen.width;// / 2.0f;
+        height = (float)Screen.height;// / 2.0f;
+
+        // Position used for the cube.
+        position = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    public void toggleMining()
+    {
+        isMining = !isMining;
+        if (rb != null)
+        {
+            if (isMining)
+            {
+                rb.isKinematic = true;
+            }
+            else
+            {
+                rb.isKinematic = false;
+            }
+        }
+    }
+
+
+    public void doJump()
+    {
+        if(rb != null && !isMining)
+        {
+            Debug.Log("Do Jump");
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+
+    }
+    void Update()
+    { 
+        // Handle screen touches.
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(isMining)
+            {
+                // Move the cube if the screen has the finger moving.
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    Vector2 pos = touch.position;
+                    Vector3 test = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, 0));
+                    Vector3 res = test - transform.position;
+                    Vector3 res2 = new Vector3(res.x, res.y, 0.0f);
+
+                    float angle = Vector3.SignedAngle(res2, Vector3.up, -Vector3.forward);
+
+                    transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+
+                }
+            }
+        }
+    }
+}
